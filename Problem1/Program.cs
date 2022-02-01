c24e0848d371ca1ff33ca4d47832e4cb2be14862
Nrkapadia <kapadianisarg052@gmail.com>
2022-02-01 15:07:51 +0530

    Final Turn

---
+++ Problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Please enter the numerator? ");
+            int numerator  = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the denominator? ");
+            int denominator = Convert.ToInt32(Console.ReadLine());
+            int quotient = numerator/denominator;
+            int remainder =  numerator%denominator;
+            Console.WriteLine($"Integer division result = {quotient} with remainder = {remainder}");
+            double fdev =(double) numerator / denominator;
+            Console.WriteLine($"Flaoting point division result = {fdev}");
+            Console.WriteLine($"The result as a mixed fraction is {quotient}({remainder}/{denominator})");
+            
+            
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //first question 
+            Console.Write("ENTER THE STRING >> ");
+            String str=Console.ReadLine();
+            Console.WriteLine($"Length of {str} is {str.Length}");
+            //second question
+            Console.WriteLine($"\nENTER THE SENTENCE >> ");
+            str=Console.ReadLine();
+            if(str.EndsWith("."))
+            {
+                Console.WriteLine("Declarative sentence");
+            }
+            else if(str.EndsWith("?"))
+            {
+                Console.WriteLine("Interrogatory sentence");   
+            }
+            else if(str.EndsWith("!"))
+            {
+                Console.WriteLine("Exclamation snetence");
+            }
+            else
+            Console.WriteLine("Not a sentence");
+
+            //third-fourth question
+            Console.Write("\nENTER YOUR NAME  >> ");
+            str =  Console.ReadLine();
+            int indexOfSpace = str.IndexOf(" ");
+            if(indexOfSpace!=-1)
+            Console.WriteLine($"WITH CHANGES  >> {str.Substring(indexOfSpace+1)}, {str.Substring(0,indexOfSpace)}");
+            else
+            Console.WriteLine("WITHOUT CHANGES >> {0}",str);
+
+
+        }
+    }
+}
